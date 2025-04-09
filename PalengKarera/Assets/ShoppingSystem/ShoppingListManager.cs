9cb09026f507fd9c11a0912c91348b9a8e846bd3
H3xaaa <148347298+H3xaaa@users.noreply.github.com>
2025-04-09 13:19:43 +0800

    Shop System

--- PalengKarera/Assets/ShoppingSystem/ShoppingListManager.cs
+++ PalengKarera/Assets/ShoppingSystem/ShoppingListManager.cs
@@ -18,7 +18,6 @@ public class ShoppingListManager : MonoBehaviour
     public List<ShoppingRequirement> shoppingRequirements = new List<ShoppingRequirement>();
     public GameObject listItemPrefab;
     public Transform listPanel;
-    public TMP_Text dishNameText;
 
     private void Awake()
     {
@@ -32,13 +31,6 @@ public class ShoppingListManager : MonoBehaviour
 
     void DisplayShoppingList()
     {
-
-        // Show dish name from the GameObject name
-        if (dishNameText != null)
-        {
-            dishNameText.text = gameObject.name;
-        }
-
         foreach (Transform child in listPanel)
         {
             Destroy(child.gameObject);
